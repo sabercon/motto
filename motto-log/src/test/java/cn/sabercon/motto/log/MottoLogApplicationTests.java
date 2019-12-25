3c972b2f3c83ae81f3fae215e336f9c1caa0d658
sabercon <894876785@qq.com>
2019-12-25 17:23:00 +0800

    first day

--- motto-log/src/test/java/cn/sabercon/motto/log/MottoLogApplicationTests.java
+++ motto-log/src/test/java/cn/sabercon/motto/log/MottoLogApplicationTests.java
@@ -1,9 +1,14 @@
 package cn.sabercon.motto.log;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class MottoLogApplicationTests {
 
     @Test
